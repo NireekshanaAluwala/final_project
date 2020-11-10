7664f983f2c4cd72b8805f084c43791272227397
NireekshanaAluwala <69748181+NireekshanaAluwala@users.noreply.github.com>
2020-11-10 05:12:51 +0000

    Add files via upload

---
+++ final_project/build/precache-manifest.7c9bafc6dedaad861294fb3c9b3d587a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f30acc94e5af822502007f075e41955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "720ede52de7263526d3f",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "ebd01207fffe903fb8f8",
+    "url": "/static/css/main.ba4cea88.chunk.css"
+  },
+  {
+    "revision": "720ede52de7263526d3f",
+    "url": "/static/js/2.c76c7867.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c76c7867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd01207fffe903fb8f8",
+    "url": "/static/js/main.28541e43.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  }
+]);
---
+++ final_project/src/component/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return(
+        <React.Fragment>
+            <footer>
+            <hr/>
+            <center>
+            <h3>&copy; Go 4 Food</h3>
+            </center>
+            </footer>
+        </React.Fragment>
+    )
+}
+
+export default Footer;
